c0a6663da1a96c96c543ff21f89fade2a8500b2f
daliphone <daliphone137@gmail.com>
2026-04-24 20:27:54 +0800

    fix: 移除 defineConfig 以繞過 Vercel 編譯器解析錯誤

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
   build: {
     // 確保支援 esnext 以解決 import.meta 的編譯錯誤
     target: 'esnext'
   }
-})
+}
