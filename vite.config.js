5c62b64cf1b89a5e0b477b00f8f7dbfc37d0fd55
daliphone <daliphone137@gmail.com>
2026-04-24 20:22:47 +0800

    fix: 確保完整引入 defineConfig 與 react 插件

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    // 確保支援 esnext 以解決 import.meta 的編譯錯誤
-    target: 'esnext'
-  }
-})
+failed to load config from /vercel/path0/vite.config.js
+error during build:
+ReferenceError: defineConfig is not defined
+    at file:///vercel/path0/vite.config.js.timestamp-1777033078354-a20e482716cc48.mjs:2:27
+    at ModuleJob.run (node:internal/modules/esm/module_job:430:25)
+    at async onImport.tracePromise.__proto__ (node:internal/modules/esm/loader:661:26)
+    at async loadConfigFromBundledFile (file:///vercel/path0/node_modules/vite/dist/node/chunks/dep-BK3b2jBa.js:66975:15)
+    at async loadConfigFromFile (file:///vercel/path0/node_modules/vite/dist/node/chunks/dep-BK3b2jBa.js:66816:24)
+    at async resolveConfig (file:///vercel/path0/node_modules/vite/dist/node/chunks/dep-BK3b2jBa.js:66416:24)
+    at async build (file:///vercel/path0/node_modules/vite/dist/node/chunks/dep-BK3b2jBa.js:65503:18)
+    at async CAC.<anonymous> (file:///vercel/path0/node_modules/vite/dist/node/cli.js:829:5)
+Error: Command "npm run build" exited with 1
